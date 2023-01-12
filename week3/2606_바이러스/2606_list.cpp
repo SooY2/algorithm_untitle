9d043caf3ea629b2f973966d0179c17ec4e6e8eb
SooY2 <101343915+SooY2@users.noreply.github.com>
2023-01-13 03:42:59 +0900

    Create 2606_list.cpp

---
+++ week3/2606_바이러스/2606_list.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+
+    int n,connectnum,ans=0,pos,poslinked;
+    int visited[101]={0};
+    
+    
+    cin>>n;
+    cin>>connectnum;
+
+    vector<int> connect[n+1];
+
+    for(int i=0;i<connectnum;i++){
+        int m,n;
+        cin>>m>>n;
+        connect[m].push_back(n);
+        connect[n].push_back(m); 
+    }
+    queue<int> q;
+    q.push(1);
+    visited[1]=1;
+
+    while (!q.empty())
+    {
+        pos=q.front();
+        q.pop();
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<connect[pos].size();j++){
+                poslinked=connect[pos][j];
+                if(visited[poslinked]!=1){
+                    q.push(poslinked);
+                    visited[poslinked]=1;
+                    ans++;
+                }
+            }
+        }
+    }
+    cout<<ans;
+
+    return 0;
+}
